66599a249809509933dc66c0faa868481ba178e9
kathia <kathia.neira@orbis.com.pe>
2019-10-07 08:45:09 -0500

    feat(components): create tag component

---
+++ src/components/Tag/config.ts
@@ -0,0 +1,71 @@
+export const tags = {
+  javascript: {
+    nameTag: 'JavaScript',
+    background: '#F0DB4F',
+    color: '#000'
+  },
+  jade: {
+    nameTag: 'Jade',
+    background: '#00a86b',
+    color: '#fff'
+  },
+  nodejs: {
+    nameTag: 'NodeJS',
+    background: '#176e00',
+    color: '#fff'
+  },
+  stylus: {
+    nameTag: 'Stylus',
+    background: '#333333',
+    color: '#fff'
+  },
+  react: {
+    nameTag: 'React',
+    background: '#61DBFB',
+    color: '#000'
+  },
+  angular: {
+    nameTag: 'Angular',
+    background: '#B52E31',
+    color: '#fff'
+  },
+  css: {
+    nameTag: 'CSS',
+    background: '#61A8FB',
+    color: '#fff'
+  },
+  html: {
+    nameTag: 'HTML',
+    background: '#F16529',
+    color: '#fff'
+  },
+  pug: {
+    nameTag: 'Pug',
+    background: '#A86454',
+    color: '#fff'
+  },
+  vue: {
+    nameTag: 'Vue',
+    background: '#41BB88',
+    color: '#000'
+  },
+  typescript: {
+    nameTag: 'TypeScript',
+    background: '#007ACC',
+    color: '#fff'
+  },
+  ruby: {
+    nameTag: 'Ruby',
+    background: '#cc0000',
+    color: '#fff'
+  },
+  linux: {
+    nameTag: 'Linux',
+    background: '#7D7D7D',
+    color: '#fff'
+  },
+  other: {
+    background: '#DDDDDD',
+    color: '#fff'
+  }
+};
