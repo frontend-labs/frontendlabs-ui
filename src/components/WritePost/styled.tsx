1cad798fde7203daffa17930036255f9312d1724
kathiaNeira <kathianeira@outlook.com>
2019-11-13 19:21:57 -0500

    feat(components): create write post component

---
+++ src/components/WritePost/styled.tsx
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  height: 200px;
+  background: #312359;
+`;
+
+export const InnerWrap = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column
+  max-width: 1800px;
+  height: 100%;
+  margin: auto;
+`;
+
+export const Title = styled.h3`
+  font-family: Nunito Sans;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 36px;
+  margin: 0;
+  margin-bottom: 13px;
+  line-height: 49px;
+  color: #FFFFFF;
+`;
+
+export const Information = styled.p`
+  font-family: Nunito Sans;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 18px;
+  line-height: 25px;
+  margin: 0;
+  color: #FFFFFF;
+`;
