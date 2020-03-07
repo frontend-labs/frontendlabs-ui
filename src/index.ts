bf91bb22a55b423e428620eea061ef62a0fd89ab
wilsson <wilsonft83@gmail.com>
2020-03-07 17:33:24 -0500

    refactor: #15 add index for export components

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export { Card } from './components/Card/Card';
+export { Cover } from './components/Cover/Cover';
+export { Footer } from './components/Footer/Footer';
+export { MiniFooter } from './components/MiniFooter/MiniFooter';
+export { Tag } from './components/Tag/Tag';
+export { WritePost } from './components/WritePost/WritePost';
